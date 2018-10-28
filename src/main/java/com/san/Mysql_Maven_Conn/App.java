3efae09858ab4a5f3e2691f3d04c61b50c259ee0
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2018-10-28 01:50:05 -0600

    pl/sql procedure called

--- src/main/java/com/san/Mysql_Maven_Conn/App.java
+++ src/main/java/com/san/Mysql_Maven_Conn/App.java
@@ -1,5 +1,6 @@
 package com.san.Mysql_Maven_Conn;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -15,13 +16,17 @@ public class App
     {
     	try{  
 			Class.forName("com.mysql.jdbc.Driver");  //or use com.mysql.jc.jdbc.Driver as new driver
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_test","test","test");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels","test","test");  
 			//here sonoo is database name, root is username and password  
-			Statement stmt=con.createStatement();  
-			ResultSet rs=stmt.executeQuery("select * from employees");  
-			while(rs.next())  
-			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-			con.close();  
+			//Statement stmt=con.createStatement();  
+			
+			CallableStatement statement = con.prepareCall("{call add_productlines()}");
+			statement.execute();
+			statement.close();
+			//ResultSet rs=stmt.executeQuery("select * from employees");  
+			//while(rs.next())  
+			//System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			 System.out.println("Stored procedure called successfully!");  
 			}
 			catch(Exception e){ 
 				System.out.println(e);  
